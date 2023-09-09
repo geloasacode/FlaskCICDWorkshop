fd7af036d78177d41a77f0c47d876bb96690850a
Gelo <gelodungo@gmail.com>
2023-09-09 20:19:36 +0800

    For deployment

---
+++ terraform/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "simple-flask-bucket"
+    key    = "./terraform.tfstate"
+    dynamodb_table = "simple-flask-dynamodb-table"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.14.0"
+    }
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,86 +1,23 @@
-# Create IAM policy
-resource "aws_iam_role_policy" "policy_ec2" {
-  name = "policy_for_ec2"
-  role = aws_iam_role.AWS-Docker-Image.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchGetImage",
-          "ec2:*"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+variable "aws-key-pair" {
+  type    = string
+  default = "terraform"
 }
 
-# Create IAM role
-resource "aws_iam_role" "AWS-Docker-Image" {
-  name = "AWS-Docker-Image"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-# Create instance profile using role
-resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_profile"
-  role = aws_iam_role.AWS-Docker-Image.name
+data "template_file" "user_data" {
+  template = file("./userdata.yml")
 }
 
 # Create ec2_instance
 resource "aws_instance" "sample-web-app" {
-  ami                  = "ami-051f7e7f6c2f40dc1"
-  iam_instance_profile = aws_iam_instance_profile.test_profile.name
-  instance_type        = "t2.micro"
-  key_name             = "terraform"
+  ami           = "ami-051f7e7f6c2f40dc1"
+  iam_instance_profile = "deploy-docker-ec2"
+  instance_type = "t2.micro"
+  key_name     = var.aws-key-pair
+  vpc_security_group_ids = []
   tags = {
-    Name : "Sample-web"
+    Name : "Ec2_instance"
   }
-
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    private_key = file("./terraform.pem")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install jq",
-      "sudo yum install docker -y",
-      "sudo service docker start",
-      "sudo usermod -aG docker ec2-user",
-      "exit", # Log out from ec2-user
-    ]
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 608569176201.dkr.ecr.us-east-1.amazonaws.com",
-      # Query image tags and store them in a variable     
-      "image_tags=$(aws ecr describe-images --repository-name gero --output json --query 'imageDetails[].imageTags[]' | jq -r 'join(" ,")')",  
-      # Get the latest image tag#      
-      "latest_tag=$(echo $image_tags | tr ' ' '\n' | sort -V | tail -n 1)",     
-      "docker pull 608569176201.dkr.ecr.us-east-1.amazonaws.com/gero:latest_tag=$(echo $image_tags | tr ' ' '\n' | sort -V | tail -n 1)",
-      "docker run -d -p 8080:8080 608569176201.dkr.ecr.us-east-1.amazonaws.com/gero:latest_tag=$(echo $image_tags | tr ' ' '\n' | sort -V | tail -n 1)"
-    ]
-  }
+  user_data = data.template_file.user_data.rendered
 }
+
+
